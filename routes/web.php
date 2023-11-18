32a652c7b190abb3d93784555006aa95df5443d6
GenuineGenius999 <lm1876356@gmail.com>
2023-11-18 00:37:22 -0800

    Fixed some errors because of the wrong methods

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,6 @@ Route::get('/category/{slug}', [ProductController::class, 'category'])->name('pr
 // Cart routes
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
-Route::post('/cart/update', [CartController::class, 'update'])->name('cart.update');
-Route::post('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
-Route::post('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
-Route::get('/cart/count', [CartController::class, 'getCartCount'])->name('cart.count');
 
 // Checkout routes
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
